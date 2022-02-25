8e01e243bc6c3bf63aebcd99fc2660f94ebdb203
Caitlin D'Souza <cd394@njit.edu>
2022-02-25 15:23:22 -0500

    workflow updates

---
+++ tests/simple_pages_test.py
@@ -0,0 +1,9 @@
+#This tests the homepage
+
+from app import create_app
+
+#def test_request_index(client):
+   # response = client.get('/')
+    #assert response.status_code == 200
+   # assert b"Index Page" in response.data
+
