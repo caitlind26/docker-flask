444d33ba4290e1d2dc2c2f764308d153e83aa8b3
Caitlin D'Souza <cd394@njit.edu>
2022-03-01 14:37:03 -0500

    proj 1

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -16,13 +16,11 @@ def test_request_index(client):
     assert response.status_code == 200
     assert b"Index Page" in response.data
 
-
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About Page" in response.data
-
 
 def test_request_page1(client):
     """This makes the index page"""
@@ -30,13 +28,11 @@ def test_request_page1(client):
     assert response.status_code == 200
     assert b"Page 1" in response.data
 
-
 def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"Page 2" in response.data
-
 
 def test_request_page3(client):
     """This makes the index page"""
@@ -44,13 +40,11 @@ def test_request_page3(client):
     assert response.status_code == 200
     assert b"Page 3" in response.data
 
-
 def test_request_page4(client):
     """This makes the index page"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"Page 4" in response.data
-
 
 def test_request_page_not_found(client):
     """This makes the index page"""
