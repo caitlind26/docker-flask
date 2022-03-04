f8a00384aa34e5c958b14ae398c3a3063ea825d0
Caitlin D'Souza <cd394@njit.edu>
2022-03-04 18:53:49 -0500

    simple pages test response

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -2,25 +2,25 @@
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
-    assert b"Index Page"
+    assert b"Index Page" in response
 
 
 def test_request_git(client):
     """This makes the index page"""
     response = client.get("/")
-    assert b"Git"
+    assert b"Git" in response
 
 def test_request_pyflask(client):
     """This makes the index page"""
     response = client.get("/")
-    assert b"Testing with pytest"
+    assert b"Testing with pytest" in response
 
 def test_request_cicd(client):
     """This makes the index page"""
     response = client.get("/")
-    assert b"Continuous Integration/Continuous Deployment"
+    assert b"Continuous Integration/Continuous Deployment" in response
 
 def test_request_docker(client):
     """This makes the index page"""
     response = client.get("/")
-    assert b"Docker"
+    assert b"Docker" in response
