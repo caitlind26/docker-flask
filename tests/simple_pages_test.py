c2ccef124b0011623d3390968e7eabeb2ab88169
Caitlin D'Souza <cd394@njit.edu>
2022-03-05 15:30:01 -0500

    paths

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -6,21 +6,21 @@
 
 
 #def test_request_git(client):
-#    """This makes the index page"""
+#    """This tests the index page"""
  #   response = client.get("/")
   #  assert b"Git" in response.data
 
-def test_request_pyflask(client):
-    """This tests the pyflask page"""
-    response = client.get("/pyflask.html")
-    assert b"Testing with pytest"
+#def test_request_pyflask(client):
+ #   """This tests the pyflask page"""
+  #  response = client.get("/app/simple_pages/templates/pyflask.html")
+ #   assert b"Testing with pytest" in response.data
 
 #def test_request_cicd(client):
-    """This makes the index page"""
+#    """This tests the cicd page"""
   #  response = client.get("/")
  #   assert b"Continuous Integration/Continuous Deployment"
 
 #def test_request_docker(client):
-    """This makes the index page"""
+ #   """This tests the docker page"""
  #   response = client.get("/")
   #  assert b"Docker"
